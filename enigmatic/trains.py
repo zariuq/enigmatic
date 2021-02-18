9af6cb36cdf58f777a514364ccbd69dac925e691
Zar Goertzel <zariuq@gmail.com>
2021-02-18 15:36:52 +0100

    fix to parents directory

--- enigmatic/trains.py
+++ enigmatic/trains.py
@@ -154,7 +154,7 @@ def make(d_posnegs, debug=[], split=False, **others):
 def build(pids, **others):
    extras = others.get("d_posneg", [])
    if others.get("parents", False):
-       d_posnegs = [os.path.join(expres.results.dir(pid=pid, **others), "parents") for pid in pids] + extras
+       d_posnegs = [os.path.join(expres.results.dir(pid=pid, **others), "parents") for pid in pids] + [os.path.join(pid, "parents") for pid in extras]
    else:
        d_posnegs = [expres.results.dir(pid=pid, **others) for pid in pids] + extras
    make(d_posnegs, **others)
